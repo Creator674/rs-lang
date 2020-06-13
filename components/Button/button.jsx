edba0cd35a4374013dfe0fcb69e14b99afef6711
Vladislav <vladislavkhlebec@gmail.com>
2020-06-13 21:11:18 +0300

    feat: base structure

---
+++ components/Button/button.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Button = props => {
+	return <button {...props}>Click me</button>
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import { Button } from '../components'
+import Link from 'next/link'
 
 function Home() {
-  return (
-    <>
-      <h1>Hello, world</h1>
-    </>
-  );
+	return (
+		<>
+			<h1>Hello, world</h1>
+			<Button className='egorka' />
+			<Button className='koll ' />
+			<Link href='/egorka'>
+				<a>Go to egorka</a>
+			</Link>
+		</>
+	)
 }
 
-export default Home;
+export default Home
