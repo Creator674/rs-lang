52e62122b77a5998d7738ec35aa70a6a02f448ca
Vladislav <vladislavkhlebec@gmail.com>
2020-06-13 22:28:21 +0300

    feat: minor updates

--- components/Button/button.jsx
+++ components/Button/button.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// import './button.less'
+
 export const Button = props => {
 	return <button {...props}>Click me</button>
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '../components'
+import { Button, Popup } from '../components'
 import Link from 'next/link'
 
 function Home() {
@@ -11,6 +11,15 @@ function Home() {
 			<Link href='/egorka'>
 				<a>Go to egorka</a>
 			</Link>
+
+			<Popup>
+				<form>
+					<input type='text' />
+					<input type='text' />
+					<input type='text' />
+				</form>
+			</Popup>
+			<img src='/images/icons/navigation/poppover.svg' alt='' />
 		</>
 	)
 }
