c924e6ef7e5eef21dc4448b4476f9b763909c004
Dmitry Hmel <dima.hmelios@gmail.com>
2020-06-14 14:17:27 +0300

    feat: add less, .env and setup basic nextjs layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,17 @@
 import React from 'react'
-import { Button, Popup } from '../components'
 import Link from 'next/link'
 
-function Home() {
-	return (
-		<>
-			<h1>Hello, world</h1>
-			<Button className='egorka' />
-			<Button className='koll ' />
-			<Link href='/egorka'>
-				<a>Go to egorka</a>
-			</Link>
+import './index.less'
 
-			<Popup>
-				<form>
-					<input type='text' />
-					<input type='text' />
-					<input type='text' />
-				</form>
-			</Popup>
-			<img src='/images/icons/navigation/poppover.svg' alt='' />
-		</>
-	)
+function Home() {
+  return (
+    <>
+      <h1>Main page</h1>
+      <Link href='/account'>
+        <a>Account</a>
+      </Link>
+    </>
+  )
 }
 
 export default Home
