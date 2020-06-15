ae2fc2586a5b89adfbcfc7520b496cbabe72769d
Dmitry Hmel <51854801+xmelsky@users.noreply.github.com>
2020-06-15 18:47:07 +0300

    Feature/popup component (#16)
    * feat: add popup component, add AOS animation library, add layout component
    
    * fix: npm and yarn lock sync
    
    * fix: modal close by Esc press. Fix prevent modal close by click on itself

--- components/Popup/popup.jsx
+++ components/Popup/popup.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './popup.less'
 
 export const Popup = (props) => {
   const { children, toggleClose } = props
+
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal)
+    return () => {
+      document.removeEventListener('keydown', closeModal)
+    }
+  }, [])
+
+  const closeModal = (e) => {
+    if (e.target.classList.contains('overlay')) {
+      toggleClose()
+    } else if (e.key === 'Escape' || e.key === 'Esc' || e.keyCode === 27) {
+      toggleClose()
+    }
+  }
+
   return (
-    <div className='overlay' onClick={toggleClose}>
+    <div className='overlay' onClick={closeModal}>
       <div className='modal' data-aos='fade-up' data-aos-delay='300'>
         {children}
       </div>
