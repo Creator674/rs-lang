ea26dcad744b9af97b3f26414800e5e517cc0b54
Kumamon <Hallygirl@mail.ru>
2020-06-27 17:20:11 +0300

    Add: api request for getting audio files

--- components/games/audiocall/audiocall.jsx
+++ components/games/audiocall/audiocall.jsx
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from 'react';  
-import { getWordsTranslationAndAudio, getImg } from '../../../lib/crud/auth'
-import { shuffledArray, shuffledRandomArray } from '../../../lib/helpers/shufflefunc';
+import { getWordsTranslationAndAudio, getImg, getAudio } from '../../../lib/crud/auth'
+import { shuffledArray } from '../../../lib/helpers/shufflefunc';
 import './audiocall.less';
-
-
 
 export const Audiocall = (props) => {
   
    let  shufeledArray1 = shuffledArray(20);
-   let  shufeledArray2 = shuffledRandomArray(20);
+   let  shufeledArray2 = shuffledArray(20);
 let isGameEnd = false;
 
-   const [countGuessed, setcountGuessed] = useState(0); 
    const [data, setDate] = useState([]);
    const [count, setCount] = useState(0);
    const [points, setPoints] = useState(0);
-   const [round, setRound] = useState(0);  
+   const [isGuessed, setIsGuessed] = useState(false);
 
    const [words, setWords] = useState();
    const [translation, setTranslation] = useState();
-   const [image, setImage] = useState(); 
+   const [images, setImages] = useState([]); 
+   const [audio, setAudio] = useState(); 
 
    const [currentTranslation, setCurrentTranslation] = useState();
    const [currentWord, setCurrentWord] = useState();
@@ -27,100 +25,117 @@ let isGameEnd = false;
    useEffect(() => {
       getWordsTranslationAndAudio(1,1)
        .then( (data) => {
-         console.log( data ) 
+         //  console.log(data);
           setDate(data);
-
-          setImage(data.map( (el) => getImg(el[3]).then(img => img)));
-
           setWords(data.map((el) => el[0]));
-          setTranslation(data.map((el) => el[1]));
+          setTranslation(data.map((el) => el[1])); 
           setCurrentWord(data[shufeledArray1[count]][0]);
-          setCurrentTranslation(data[shufeledArray2[count]][1]);
+          setCurrentTranslation(data[shufeledArray1[count]][1]);
           
+          const images = data.map( (el) => getImg(el[3]).then(img => img));
+          Promise.allSettled(images).then((results) => {
+             setImages(results.map((res) => res.value))
+          });
+          const audio = data.map( (el) => getAudio(el[2]).then(el => el));
+          Promise.allSettled(audio).then((results) => {
+             setAudio(results.map((res) => res.value))
+          });
           return data;
         })
         .catch((error) => error);
    }, []);
 
-   console.log( image );
 
-   const handleClick = (bool) => {  
-
-      const answer = words.indexOf(currentWord) === translation.indexOf(currentTranslation); 
-      if(bool === answer){
-         console.log('yes, it"s" true!!');
-         setPoints(points + 10);
-         setcountGuessed(countGuessed + 1);
-         document.getElementById('yes').play();
-      } else {
-         setcountGuessed(0);
-         console.log('no, it"s false!!');
-         document.getElementById('no').play();
-      }
-      setCount(count + 1); 
-
-      if(countGuessed && countGuessed % 5 === 0){
-         document.getElementById('wow').play();
-         document.getElementById('points').classList.add('wow');
-         setTimeout(() => {
-            document.getElementById('points').classList.remove('wow');
-         }, 1100);
-         setPoints(points + 50);
-      }  
-
-       if(count + 1 > 19){
-         setCount(0);
-         shufeledArray1 = shuffledArray(20);
-         shufeledArray2 = shuffledRandomArray(20);
-         setRound(round + 1); 
-       }
-       setCurrentWord(words[shufeledArray1[count]]);
-       setCurrentTranslation(translation[shufeledArray2[count]]);
-
-       if(round === 3){
-         isGameEnd = true;
-         console.log("it's the end of game!");
-         //   ToDo: the end of game
-       }
-
-       console.log(currentWord, currentTranslation)
-
-   }
-
-   const drawWords = () => {
-         return (
+   const drawWords = () => { 
+console.log(images[shufeledArray1[count]]);
+      const arrayOfFiveWords = [];
+      arrayOfFiveWords.push(currentTranslation);
+      let i = 0;
+      // while (arrayOfFiveWords.length < 5) {
+      //    const ind = shufeledArray2[i]; 
+      //    const currentInd = shufeledArray1[count];
+      //    if(i !== currentInd){
+      //       arrayOfFiveWords.push(translation[ind]);
+      //    }
+      //    i += 1;
+      // }
+      arrayOfFiveWords.sort(() => Math.random() - 0.5);
+         return arrayOfFiveWords.map( (el, i) => (
             <div className="word-box">
-               <span className="number">{''}</span>
-               <div key={''} className="word">
-                  {''}
+               <span className="number">{i + 1}</span>
+                  <div key={i} className="word"
+                              onClick={() => handleClick(el)} >
+                  {el}
                </div>
             </div>
-         );
-   }
-    
+         ));
+   };
+console.log(images) 
+console.log(audio) 
+
+   const handleClick = (word) => {  
+      if(currentTranslation === word){
+         console.log('yes, it"s" true!!');
+         setPoints(points + 10);
+         setIsGuessed(true);
+      } else {
+         console.log('no, it"s false!!');
+      }
+      setCount(count + 1); 
+      shufeledArray2 = shuffledArray(20);  // refreshing shuffled array
+      if(count + 1 > 19){
+         console.log('THE END OF GAME');
+         isGameEnd = true;
+      }
+
+      setCurrentWord(words[shufeledArray1[count]]);
+      setCurrentTranslation(translation[shufeledArray1[count]]);
+      setCurrentImage(images[shufeledArray1[count]]);
+      setCurrentAudio(audio[shufeledArray1[count]]);
+      console.log(currentWord, currentTranslation)
+   };
    
+   const audioPlay = () => {
+      document.getElementById('word').play();
+      
+   };
+
+   const showAnswer = () => {
+      console.log(currentTranslation);
+      setIsGuessed(true);
+      setTimeout(()=>{
+         setIsGuessed(false);
+      }, 1200);
+   };
+
       return (
          <div className="wrapper">
             <div className="darken">
                <div className="icon">
                   <img src="/images/audiocall/play.png" alt="pic" 
                         width="70"
-                        onClick={()=> console.log("clicked")}/>
+                        onClick={() => audioPlay() }
+                        />
                </div>
                <div className="points">
-                  <span>POINTS:</span> 100500
+                  <span>POINTS:</span> {points}
+                  <div className="image">
+                     <img src={isGuessed ? '' : '/images/audiocall/snail.png'} alt="word"></img>
+                  </div>
+                  <p className={`word-correct ${isGuessed ? images[shufeledArray1[count]] : 'hide'} `} >{currentWord}</p>
                </div>
                <div className="cards">
                   { drawWords() }      
                </div>
                <div className="button">
-                  <button className="btn">I don't know</button>
+                  <button className="btn"
+                          onClick={() => showAnswer()}
+                          >
+                  I don't know</button>
                </div>
          </div>
 
-            <audio src='/audio/kok.mp3' className="audio_word" id="yes"></audio>
-            <audio src='/audio/wow.mp3' className="audio_word" id="wow"></audio>
+            {/* <audio src={audio[shufeledArray1[count]] || '/audio/wow.mp3'} id="word"></audio> */}
          </div>
       )
-   
 } 
