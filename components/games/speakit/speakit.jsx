2fff44a93a096d63fe8202137b5087d9731587c2
Kumamon <Hallygirl@mail.ru>
2020-06-29 19:39:44 +0300

    Add: states & useStates

--- components/games/speakit/speakit.jsx
+++ components/games/speakit/speakit.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Header } from './header'
 import { TenCards } from './tencards'
 import { CardShow } from './cardshow'
 import './index.less'
+import {getWordsTranslationAudioImg} from '../../../lib/crud/auth';
+
 
 export function Speakit(props) {
+
+  const [word, setWord] = useState();
+  const [translation, setTranslation] = useState();
+  const [audio, setAudio] = useState();
+  const [image, setImage] = useState();
+
+
+  useEffect(() => {
+    getWordsTranslationAudioImg(1,1).then((data) => {
+      console.log(data)
+    });
+  });
 
   return (
     <div className='wrapper-speakit'>
