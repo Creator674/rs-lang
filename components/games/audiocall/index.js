cc829a2d03cdb19c41330cb19d50334cd19214a9
Dmitry Hmel <51854801+xmelsky@users.noreply.github.com>
2020-07-07 17:08:25 +0200

    Audiocall game and layout changes (#111)
    * Init: started creating fetch data
    
    * Add: useeffect functionality
    
    * Add: api request for getting audio files
    
    * Add: corrections to style, functions
    
    * refactor: audiocall components
    
    * refactor: get rid of unnecessary useState
    
    * feat: add favicon, refactor: audiocall components
    
    * feat: add randomList and buttons. setup play logic
    
    * feat: add guess buttons and setup track answer logic
    
    * feat: add event listeners for key press, add sound animation
    
    * feat: add transition group, setup basic styles
    
    * refactor: button-list component
    
    * refactor: change pages structure
    
    * refactor: remove unnecessary files
    
    Co-authored-by: Kumamon <Hallygirl@mail.ru>
    Co-authored-by: xmelsky <xmelsky@gmail.com>

--- components/games/audiocall/index.js
+++ components/games/audiocall/index.js
@@ -1,1 +1,3 @@
 export * from './audiocall'
+export * from './button-audio'
+export * from './buttons-list'
---
+++ pages/learn/index.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from 'react'
+import Link from 'next/link'
+import { AppLayout } from 'layouts'
+// import './index.less'
+
+import { ProgressBar, ProgressChart } from 'components/Progress'
+import { Menu } from 'components/Menu'
+import { Audiocall } from 'components/games'
+import { Button, Popup, SignIn, SignUp, UserAvatar } from 'components'
+
+function Home() {
+  const [showSignUp, toggleSignUp] = useState(false)
+  const [showSignIn, toggleSignIn] = useState(false)
+
+  const [testValue, setTestValue] = useState(0)
+  return (
+    <AppLayout>
+      {/* <Audiocall />    */}
+      {/* <Savannah /> */}
+
+      <div className='wrapper-main-page'>
+        <Menu />
+        <UserAvatar />
+      </div>
+
+      <ProgressBar current={36} total={50} width='60%' />
+      <ProgressChart value={testValue} width='48px' />
+
+      <input
+        type='text'
+        value={testValue}
+        onChange={({ target: { value } }) => {
+          setTestValue(+value || 0)
+        }}
+      />
+      <h1>Main page</h1>
+      <Button className='danger' onClick={() => console.log('triggered')}>
+        Danger button
+      </Button>
+      <Button className='default' onClick={() => toggleSignUp(!showSignUp)}>
+        Sign Up
+      </Button>
+      <Button className='log-in' onClick={() => toggleSignIn(!showSignIn)}>
+        <div></div>
+      </Button>
+      {showSignUp ? (
+        <Popup toggleClose={() => toggleSignUp(false)}>
+          <div className='register-form'>
+            <SignUp />
+          </div>
+        </Popup>
+      ) : showSignIn ? (
+        <Popup toggleClose={() => toggleSignIn(false)}>
+          <div className='register-form'>
+            <SignIn />
+          </div>
+        </Popup>
+      ) : null}
+      <Link href='/account'>
+        <a>Account</a>
+      </Link>
+    </AppLayout>
+  )
+}
+
+export default Home
