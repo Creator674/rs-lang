13b773be0be8e7faf37067c9a801ef83966e11f3
Olga Kharkevich <60240612+olgakharkevich@users.noreply.github.com>
2020-07-09 10:34:47 +0300

    121 fix/styles login and landing card (#122)
    * fix: styles button login, add grass
    
    * fix: styles landingCard

---
+++ components/ButtonLogIn/buttonLogIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './buttonLogIn.less'
+
+export const ButtonLogIn = () => {
+
+  return (
+    <button className='login'>
+      <span className='login__line'></span>
+      <span className='login__round'></span>
+      Log&nbsp;in
+    </button>
+  )
+}
