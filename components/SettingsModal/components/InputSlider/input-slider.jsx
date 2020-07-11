0e6cd51405ca1bd777277a5e3499cfc6b5742bb1
Yaroslav Ryzhkov <43150028+Creator674@users.noreply.github.com>
2020-07-11 09:27:26 +0300

    Feature/settings modal (#127)
    * refactor: implement modal window (settings)
    
    * refactor: delete useless code
    
    * feat: implement settings modal window
    
    * feat: add logic to radio buttons
    
    * feat: add basic logic
    
    Co-authored-by: Kuma_Mon <57950327+davinchick@users.noreply.github.com>

---
+++ components/SettingsModal/components/InputSlider/input-slider.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+import './input-slider.less'
+
+export const InputSlider = (props) => {
+
+    const { labelText } = props
+
+    const [value, setValue] = useState(5)
+
+    const handleNumberChange = (event) => {
+        setValue(event.target.value === '' ? '' : Number(event.target.value))
+    }
+
+
+
+    const handleBlur = () => {
+        if (value < 0) {
+            setValue(0)
+        } else if (value > 30) {
+            setValue(30)
+        }
+    }
+
+    return (
+        <li className="rangeInputSliderWrapper">
+            <input
+                className="rangeInput"
+                type="number"
+                min="5"
+                max="30"
+                step="1"
+                value={typeof value === 'number' ? value : 10}
+                onChange={handleNumberChange}
+                onBlur={handleBlur}
+            />
+
+            <div className="rangeSliderWrapper">
+                <input
+                    className="rangeSlider"
+                    type="range"
+                    min="5"
+                    max="30"
+                    step="1"
+                    onChange={handleNumberChange}
+                    value={typeof value === 'number' ? value : 10}
+                    id="input-slider"
+                />
+                <label className="sliderText" htmlFor="input-slider">{labelText}</label>
+            </div>
+
+        </li>
+    )
+}
